e56b013f9a02a459dd5a6fcad6fdf50766ab0247
Kamn <kamn@users.noreply.github.com>
2016-11-04 23:05:28 -0600

    Day 4

---
+++ log/Day-4.md
@@ -0,0 +1,9 @@
+# Day 4 -
+
+I just forked and created my own repo based on strips.
+Should of done this last night.
+Much easier.
+
+
+Not a lot else.
+Was playing games :/
--- src/ashen/strips.cljs
+++ src/ashen/strips.cljs
@@ -10,10 +10,8 @@
   (:requirements :strips)
   (:action move
      :parameters (?b ?t1 ?t2)
-     :precondition (and (block ?b) (table ?t1) (table ?t2) (on ?b ?t1) (not (on ?b ?t2))
-     :effect (and (on ?b ?t2)) (not (on ?b ?t1))))
-     )")
-
+     :precondition (and (block ?b) (table ?t1) (table ?t2) (on ?b ?t1) (not (on ?b ?t2)))
+     :effect (and (on ?b ?t2) (not (on ?b ?t1)))))")
 
 (def example-problem "(define (problem move-blocks-from-a-to-b)
       (:domain blocksworld)
@@ -22,7 +20,82 @@
     (:goal (and (on a y) (on b y)))
   )")
 
+(def example-domain2 "(define (domain starcraft)
+  (:requirements :strips :typing)
+  (:types builder building area)
+  (:action move
+     :parameters (?b - builder ?t1 - area ?t2 - area)
+     :precondition (and (scv ?b) (location ?t1) (location ?t2) (at ?b ?t1) (not (at ?b ?t2))
+     :effect (and (at ?b ?t2)) (not (at ?b ?t1))))
+  (:action collect-minerals
+     :parameters (?b - builder ?t1 - area)
+     :precondition (and (scv ?b) (location ?t1) (minerals ?t1) (not (empty ?t1)) (at ?b ?t1))
+     :effect (and (empty ?t1) (collected-minerals ?b)))
+  (:action build-supply-depot
+     :parameters (?b - builder ?t1 - area)
+     :precondition (and (scv ?b) (location ?t1) (collected-minerals ?b) (at ?b ?t1) not (building ?t1) not (minerals ?t1))
+     :effect (and (building ?t1) (depot ?t1) not (collected-minerals ?b)))
+  (:action build-barracks
+     :parameters (?b - builder ?t1 - area ?t2 - area)
+     :precondition (and (scv ?b) (location ?t1) (location ?t2) (depot ?t2) (collected-minerals ?b) (at ?b ?t1) not (building ?t1) not (minerals ?t1))
+     :effect (and (building ?t1) (barracks ?t1) not (collected-minerals ?b)))
+  (:action build-factory
+     :parameters (?b - builder ?t1 - area  ?t2 - area)
+     :precondition (and (scv ?b) (location ?t1) (location ?t2) (barracks ?t2) (collected-minerals ?b) (at ?b ?t1) not (building ?t1) not (minerals ?t1))
+     :effect (and (building ?t1) (factory ?t1) not (collected-minerals ?b)))
+  (:action build-starport
+     :parameters (?b - builder ?t1 - area  ?t2 - area)
+     :precondition (and (scv ?b) (location ?t1) (location ?t2) (factory ?t2) (collected-minerals ?b) (at ?b ?t1) not (building ?t1) not (minerals ?t1))
+     :effect (and (building ?t1) (starport ?t1) not (collected-minerals ?b)))
+  (:action build-fusion-core
+     :parameters (?b - builder ?t1 - area  ?t2 - area)
+     :precondition (and (scv ?b) (location ?t1) (location ?t2) (starport ?t2) (collected-minerals ?b) (at ?b ?t1) not (building ?t1) not (minerals ?t1))
+     :effect (and (building ?t1) (fusion-core ?t1) not (collected-minerals ?b)))
+  (:action train-marine
+     :parameters (?b - builder ?t1 - area)
+     :precondition (and (collected-minerals ?b) (barracks ?t1))
+     :effect (and (marine ?t1) not (collected-minerals ?b)))
+  (:action train-tank
+     :parameters (?b - builder ?t1 - area)
+     :precondition (and (collected-minerals ?b) (factory ?t1))
+     :effect (and (tank ?t1) not (collected-minerals ?b)))
+  (:action train-wraith
+     :parameters (?b - builder ?t1 - area)
+     :precondition (and (collected-minerals ?b) (starport ?t1))
+     :effect (and (wraith ?t1) not (collected-minerals ?b)))
+  (:action train-battlecruiser
+     :parameters (?b - builder ?t1 - area ?t2 - area)
+     :precondition (and (collected-minerals ?b) (starport ?t1) (fusion-core ?t2))
+     :effect (and (battlecruiser ?t1) not (collected-minerals ?b)))
+)")
+
+(def example-problem2 "(define (problem build-barracks)
+  (:domain starcraft)
+  (:objects
+    scv - builder
+    sectorA sectorB mineralFieldA mineralFieldB - area)
+  (:init (and (scv scv) (location sectorA) (location sectorB)
+         (location mineralFieldA) (location mineralFieldB)
+         (minerals mineralFieldA) (minerals mineralFieldB)
+         (at scv sectorA)))
+  (:goal (and (barracks sectorA)))
+  )")
+
+(def global-domain
+  {:domain "ashen"
+   :requirements :strips
+   :actions
+      [
+        {:name "move"
+         :parameters [:?b :?t1 :?t2]
+         :precondition [:and [:block :?b]
+                             [:table :?t1]
+                             [:table :?t2]
+                             [:on :?b :?t1]
+                             [:not [:on :?b :?t2]]]
+         :effect [:and [:on :?b :?t2] [:not [:on :?b :?t1]]]}]})
+
 (defn cost [state] 10)
 
 (defn solve-simple []
-  (println (.parseAndSolve strips example-domain example-problem cost)))
+  (println (.parseAndSolve strips example-domain2 example-problem2 cost)))
